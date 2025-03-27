ae1b73e3b4ff7dc572de5ca8ac3e752dff8fade0
Gokul <gokul@cerenode.io>
2025-03-27 08:47:14 +0530

    service proxy require dataobject instead of record

--- src/main/java/org/cdpg/dx/aaa/organization/service/package-info.java
+++
@@ -1,5 +0,0 @@
-@ModuleGen(groupPackage = "iudx.aaa.server.organization.service",
-  name = "iudx-aaa-server-organization-service", useFutures = true)
-package org.cdpg.dx.aaa.organization.service;
-
-import io.vertx.codegen.annotations.ModuleGen;
