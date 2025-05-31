954ddf0f705a69e0331cab4a2ac5b54924284883
srishti <srishti.mittal@datakaveri.org>
2025-05-31 09:25:09 +0530

    Working email test code

---
+++ src/main/java/org/cdpg/dx/aaa/email/service/package-info.java
@@ -0,0 +1,8 @@
+@ModuleGen(
+  groupPackage = "org.cdpg.dx.aaa.email.service",
+  name = "org-cdpg-dx-aaa-email-service",
+  useFutures = true
+)
+package org.cdpg.dx.aaa.email.service;
+
+import io.vertx.codegen.annotations.ModuleGen;
