b583d268ff7696dbc3bcc7d2ddac0a45a90bdad0
srishti <srishti.mittal@datakaveri.org>
2025-06-02 16:13:49 +0530

    Add html template

--- src/test/java/org/cdpg/dx/aaa/email/EmailVerticleTest.java
+++ src/test/java/org/cdpg/dx/aaa/email/EmailVerticleTest.java
@@ -11,10 +11,17 @@ import org.cdpg.dx.aaa.email.models.EmailRequest;
 import org.cdpg.dx.aaa.email.service.EmailService;
 import org.cdpg.dx.aaa.email.service.EmailServiceImpl;
 import org.cdpg.dx.aaa.email.verticle.EmailVerticle;
+import org.cdpg.dx.common.exception.DxBadRequestException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
 
 import static org.cdpg.dx.common.config.ServiceProxyAddressConstants.EMAIL_SERVICE_ADDRESS;
 
@@ -28,10 +35,10 @@ public class EmailVerticleTest {
   void setup(Vertx vertx,VertxTestContext testContext) {
 
     JsonObject config = new JsonObject()
-      .put("host", "hostname")
-      .put("port", 2587)  // or 2587 based on SES
-      .put("username", "username")
-      .put("password", "password");
+      .put("host", "email-smtp.ap-south-1.amazonaws.com")
+      .put("port", 2587)
+      .put("username", "AKIAROAMFPEM2M4QITED")
+      .put("password", "BE6v8Dw3LC/snQ/JEJh4aKwo2gWkCGvtNPh9axF6F86r");
 
     vertx.deployVerticle(new EmailVerticle(), new DeploymentOptions().setConfig(config))
       .onSuccess(id -> testContext.completeNow())
@@ -43,14 +50,35 @@ public class EmailVerticleTest {
     EmailService emailService = EmailService.createProxy(vertx, EMAIL_SERVICE_ADDRESS);
 
     EmailRequest request = new EmailRequest();
-    request.setFrom("no-reply.dev@iudx.io"); // ✅ Must be verified in SES
+    request.setFrom("no-reply.dev@iudx.io"); // Must be verified in SES
     request.setTo("sample_email@gmail.com");
     request.setSubject("Vert.x Email Test");
     request.setText("This is a test email sent from a deployed EmailVerticle.");
 
-    emailService.sendEmail(request)
+
+    String htmlTemplate = "";
+    try {
+      htmlTemplate = Files.readString(
+              Path.of("src/main/java/org/cdpg/dx/templates/request-create-organization.html"),
+              StandardCharsets.UTF_8
+      );
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+
+    try {
+      htmlTemplate = htmlTemplate.replace("${username}", "username")
+              .replace("${orgname}", "orgname");
+    }
+    catch(DxBadRequestException e)
+    {
+      throw new DxBadRequestException("Parameters replacement in html invalid !!");
+    }
+
+    emailService.sendEmail(request,htmlTemplate)
       .onSuccess(res -> {
-        System.out.println("✅ Email sent successfully");
+        System.out.println("Email sent successfully");
         testContext.completeNow();
       })
       .onFailure(err -> {
