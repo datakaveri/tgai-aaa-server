4510621c1737defaf54e7f540ce7b35a97d15647
srishti <srishti.mittal@datakaveri.org>
2025-06-03 12:10:37 +0530

    Add organisation_documents field

--- src/main/java/org/cdpg/dx/aaa/organization/models/OrganizationCreateRequest.java
+++ src/main/java/org/cdpg/dx/aaa/organization/models/OrganizationCreateRequest.java
@@ -31,6 +31,7 @@ public record OrganizationCreateRequest(
         String empId,
         String jobTitle,
         String orgManagerphoneNo,
+        String orgDocuments,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
 ) implements BaseEntity<OrganizationCreateRequest> {
@@ -58,7 +59,8 @@ public record OrganizationCreateRequest(
         requireNonNull(json.getString(Constants.EMP_ID), Constants.EMP_ID),
               requireNonNull(json.getString(Constants.JOB_TITLE), Constants.JOB_TITLE),
               requireNonNull(json.getString(Constants.PHONE_NO), Constants.PHONE_NO),
-              parseDateTime(json.getString(Constants.CREATED_AT)),
+        json.getString(Constants.ORG_DOCUMENTS),
+        parseDateTime(json.getString(Constants.CREATED_AT)),
               parseDateTime(json.getString(Constants.UPDATED_AT))
       );
     } catch (IllegalArgumentException e) {
@@ -86,6 +88,7 @@ public record OrganizationCreateRequest(
     json.put(Constants.EMP_ID, empId);
     json.put(Constants.JOB_TITLE, jobTitle);
     json.put(Constants.PHONE_NO, orgManagerphoneNo);
+    if (orgDocuments != null && !orgDocuments.isEmpty()) json.put(Constants.ORG_DOCUMENTS, orgDocuments);
     if (createdAt != null) json.put(Constants.CREATED_AT, createdAt.format(FORMATTER));
     if (updatedAt != null) json.put(Constants.UPDATED_AT, updatedAt.format(FORMATTER));
 
@@ -111,6 +114,7 @@ public record OrganizationCreateRequest(
     if (!empId.isEmpty()) map.put(Constants.EMP_ID, empId);
     if (!jobTitle.isEmpty()) map.put(Constants.JOB_TITLE, jobTitle);
     if (!orgManagerphoneNo.isEmpty()) map.put(Constants.PHONE_NO, orgManagerphoneNo);
+    if (orgDocuments != null && !orgDocuments.isEmpty()) map.put(Constants.ORG_DOCUMENTS, orgDocuments);
     if (createdAt != null) map.put(Constants.CREATED_AT, createdAt.format(FORMATTER));
     if (updatedAt != null) map.put(Constants.UPDATED_AT, updatedAt.format(FORMATTER));
 
