c985c923d7104be26e8f928b07b4abb319059937
srishti <srishti.mittal@datakaveri.org>
2025-06-27 11:56:08 +0530

    Update OrgUser and OrgJoinReq with officialEmail

---
+++ src/main/resources/db/migration/V13__Alter_OrganizationUser_table.sql
@@ -0,0 +1,23 @@
+ALTER TABLE organization_users
+ADD COLUMN official_email VARCHAR;
+
+UPDATE organization_users
+SET official_email = CONCAT('temp_email_', id, '@example.com');
+
+ALTER TABLE organization_users
+ALTER COLUMN official_email SET NOT NULL;
+
+ALTER TABLE organization_users
+ADD CONSTRAINT unique_official_email UNIQUE (official_email);
+
+ALTER TABLE organization_join_requests
+ADD COLUMN official_email VARCHAR;
+
+UPDATE organization_join_requests
+SET official_email = CONCAT('temp_email_', id, '@example.com');
+
+ALTER TABLE organization_join_requests
+ALTER COLUMN official_email SET NOT NULL;
+
+ALTER TABLE organization_join_requests
+ADD CONSTRAINT unique_official_email_join_request UNIQUE (official_email);
