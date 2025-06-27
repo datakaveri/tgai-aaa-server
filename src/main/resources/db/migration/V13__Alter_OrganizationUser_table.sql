20b0af971f30d697e74aa09159cb6a19a392bbcc
Gokul <gokul@cerenode.io>
2025-06-27 13:13:00 +0530

    removed unique constraint

--- src/main/resources/db/migration/V13__Alter_OrganizationUser_table.sql
+++ src/main/resources/db/migration/V13__Alter_OrganizationUser_table.sql
@@ -18,6 +18,3 @@ SET official_email = CONCAT('temp_email_', id, '@example.com');
 
 ALTER TABLE organization_join_requests
 ALTER COLUMN official_email SET NOT NULL;
-
-ALTER TABLE organization_join_requests
-ADD CONSTRAINT unique_official_email_join_request UNIQUE (official_email);
