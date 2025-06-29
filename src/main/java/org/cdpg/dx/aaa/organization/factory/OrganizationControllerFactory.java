bf8ae5886b2d066c906283e247bd51b1c9dd05c0
Rohith <rohithvaidya@gmail.com>
2025-06-29 13:37:43 +0530

    adding csv reports module

--- src/main/java/org/cdpg/dx/aaa/organization/factory/OrganizationControllerFactory.java
+++ src/main/java/org/cdpg/dx/aaa/organization/factory/OrganizationControllerFactory.java
@@ -1,14 +1,19 @@
 package org.cdpg.dx.aaa.organization.factory;
 
+import io.vertx.core.Vertx;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import org.cdpg.dx.aaa.credit.dao.ComputeRoleDAO;
+import org.cdpg.dx.aaa.credit.dao.CreditDAOFactory;
 import org.cdpg.dx.aaa.email.util.EmailComposer;
 import org.cdpg.dx.aaa.organization.controller.OrganizationController;
 import org.cdpg.dx.aaa.organization.dao.OrganizationDAOFactory;
 import org.cdpg.dx.aaa.organization.handler.OrganizationHandler;
 import org.cdpg.dx.aaa.organization.service.OrganizationService;
 import org.cdpg.dx.aaa.organization.service.OrganizationServiceImpl;
+import org.cdpg.dx.aaa.report.service.OrganizationCreateReportService;
+import org.cdpg.dx.aaa.report.service.impl.OrganizationCreateRequestReportServiceImpl;
 import org.cdpg.dx.aaa.user.service.UserService;
 import org.cdpg.dx.auditing.handler.AuditingHandler;
 import org.cdpg.dx.database.postgres.service.PostgresService;
@@ -21,10 +26,12 @@ public class OrganizationControllerFactory {
 
     private OrganizationControllerFactory() {}
 
-    public static OrganizationController create(OrganizationService organizationService, UserService userService, AuditingHandler auditingHandler,EmailComposer emailComposer) {
+    public static OrganizationController create(OrganizationService organizationService, UserService userService, AuditingHandler auditingHandler, EmailComposer emailComposer, Vertx vertx, PostgresService pgService) {
 
-
-        OrganizationHandler  organizationHandler = new OrganizationHandler(organizationService, userService,emailComposer);
+        OrganizationDAOFactory organizationDAOFactory = new OrganizationDAOFactory(pgService);
+        CreditDAOFactory creditDAOFactory = new CreditDAOFactory(pgService);
+        OrganizationCreateReportService organizationCreateReportService = new OrganizationCreateRequestReportServiceImpl(organizationDAOFactory, creditDAOFactory, vertx);
+        OrganizationHandler  organizationHandler = new OrganizationHandler(organizationService, userService,emailComposer, organizationCreateReportService);
         return new OrganizationController(organizationHandler, auditingHandler);
     }
 
---
+++ src/main/java/org/cdpg/dx/aaa/report/service/OrganizationCreateReportService.java
@@ -0,0 +1,23 @@
+package org.cdpg.dx.aaa.report.service;
+
+import io.vertx.core.Future;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.streams.ReadStream;
+import org.cdpg.dx.aaa.organization.dao.OrganizationCreateRequestDAO;
+import org.cdpg.dx.common.request.PaginatedRequest;
+
+public interface OrganizationCreateReportService {
+
+  Future<ReadStream<Buffer>> streamAdminCsvBatchedCreateRequest(PaginatedRequest request);
+
+  Future<ReadStream<Buffer>> streamConsumerCsvBatched(PaginatedRequest request);
+
+  Future<ReadStream<Buffer>> streamAdminCsvBatchedJoinRequest(PaginatedRequest request);
+
+  Future<ReadStream<Buffer>> streamAdminCsvBatchedProviderRequest(PaginatedRequest request);
+
+  Future<ReadStream<Buffer>> streamAdminCsvBatchedComputeRequest(PaginatedRequest request);
+
+  Future<ReadStream<Buffer>> streamAdminCsvBatchedOrganization(PaginatedRequest request);
+
+}
